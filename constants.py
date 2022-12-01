89d2396be75e7eb968c5bcaf2e9ebc47da1ee264
Viktor Schlegel <viktor_schlegel@asus.com>
2022-12-01 07:18:32 +0000

    add canged constants file

--- constants.py
+++ constants.py
@@ -5,6 +5,6 @@ MAX_LENGTH = 2500
 # where you want to save any models you may train
 MODEL_DIR = "/mnt/viktor_schlegel/caml-models"
 
-DATA_DIR = "/mnt/viktor_schlegel/mimic3"
-MIMIC_3_DIR = "/mnt/viktor_schlegel/mimic3"
+DATA_DIR = "data/"
+MIMIC_3_DIR = "data/"
 MIMIC_2_DIR = "/mny/viktor_schlegel/caml-mimic/mimicdata/mimic2"
